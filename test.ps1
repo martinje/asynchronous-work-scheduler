5c338f810345b347c4135cc1bebe3e8320bcad76
Martin Jentoft <mj@duett.no>
2024-05-02 15:43:20 +0200

    Add powershell script for testing.

--- test.ps1
+++ test.ps1
@@ -7,9 +7,20 @@ $id = $response.id
 # Display the ID
 Write-Host "Work request submitted with ID: $id"
 
-# Check the status of the work request using the ID
-$status_response = Invoke-RestMethod -Method Get -Uri ("http://localhost:5000/api?id=$id")
+# Loop to continuously poll the status
+while ($true) {
+    # Check the status of the work request using the ID
+    $status_response = Invoke-RestMethod -Method Get -Uri ("http://localhost:5000/api?id=$id")
 
-# Display the status response
-Write-Host "Status of work request with ID $($id):"
-$status_response
+    # Display the status response
+    Write-Host "Status of work request with ID $($id): $($status_response)"
+
+    # If the status is "done", stop polling
+    if ($status_response.message -eq "Job is done!") {
+        Write-Host "Work request with ID $($id) is done."
+        break
+    }
+
+    # Wait for ten seconds before polling again
+    Start-Sleep -Milliseconds 10000
+}
